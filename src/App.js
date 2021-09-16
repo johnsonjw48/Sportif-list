d92850f04beae895d3093a711daba0549a984032
johnsonjw48 <73818994+johnsonjw48@users.noreply.github.com>
2021-09-16 21:01:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [code, setCode] = useState("");
+  const [adresse, setAdresse] = useState("");
+
+  const [tab, setTab]= useState([]);
+
+  useEffect(() => {
+    console.log(tab);
+  }, [tab]);
+
+
+
+  function add(e) {
+    e.preventDefault();
+
+    const id = Date.now();
+
+    const obj = {id, nom, prenom, code, adresse};
+
+    let  tab1 = tab.slice();
+
+    let count = tab1.push(obj);
+
+    setTab(tab1);
+
+    
+
+
+  }
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+        <div className="titre">
+            <div className="le-titre"><h1>Mes coachs</h1></div>
+        </div>
+
+          <form className="formulaire-ajout" onSubmit={add}>
+            <div>
+              <input type="text" placeholder="Nom"  onChange={(e)=>setNom(e.target.value)} />
+
+              <input type="text" placeholder="Code postal"  onChange={(e)=>setCode(e.target.value)} />
+            </div>
+
+            <div>
+              <input type="text" placeholder="Prenom" onChange={(e)=>setPrenom(e.target.value)} />
+
+              <input type="text" placeholder="Adresse" onChange={(e)=>setAdresse(e.target.value)} />
+            </div>
+                <button>Ajouter</button> 
+          </form>
+
+          {tab.map((item)=>(
+          
+          <div className="list-coach">
+
+              <ul>
+                  
+                     <li></li>
+              
+              </ul>
+
+          </div> ))}
+      
     </div>
   );
 }
